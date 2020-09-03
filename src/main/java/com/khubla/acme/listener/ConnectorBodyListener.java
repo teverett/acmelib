84bc0fc0a6f3379b131bcf3a8f606c21b638ccd1
Tom Everett <tom@khubla.com>
2020-09-03 13:34:01 -0600

    more

--- src/main/java/com/khubla/acme/listener/ConnectorBodyListener.java
+++ src/main/java/com/khubla/acme/listener/ConnectorBodyListener.java
@@ -5,6 +5,29 @@ import com.khubla.acme.acmeParser.*;
 public class ConnectorBodyListener extends AbstractListener {
 	@Override
 	public void enterAcmeConnectorBody(AcmeConnectorBodyContext ctx) {
-		throw new RuntimeException("Not Implemented");
+		if (null != ctx.acmePropertyDeclaration()) {
+			for (AcmePropertyDeclarationContext acmePropertyDeclarationContext : ctx.acmePropertyDeclaration()) {
+				PropertyDeclarationListener propertyDeclarationListener = new PropertyDeclarationListener();
+				propertyDeclarationListener.enterAcmePropertyDeclaration(acmePropertyDeclarationContext);
+			}
+		}
+		if (null != ctx.acmeRepresentationDeclaration()) {
+			for (AcmeRepresentationDeclarationContext acmeRepresentationDeclarationContext : ctx.acmeRepresentationDeclaration()) {
+				RepresentationDeclarationListener representationDeclarationListener = new RepresentationDeclarationListener();
+				representationDeclarationListener.enterAcmeRepresentationDeclaration(acmeRepresentationDeclarationContext);
+			}
+		}
+		if (null != ctx.acmeRoleDeclaration()) {
+			for (AcmeRoleDeclarationContext acmeRoleDeclarationContext : ctx.acmeRoleDeclaration()) {
+				RoleDeclarationListener roleDeclarationListener = new RoleDeclarationListener();
+				roleDeclarationListener.enterAcmeRoleDeclaration(acmeRoleDeclarationContext);
+			}
+		}
+		if (null != ctx.designRule()) {
+			for (DesignRuleContext designRuleContext : ctx.designRule()) {
+				DesignRuleListener designRuleListener = new DesignRuleListener();
+				designRuleListener.enterDesignRule(designRuleContext);
+			}
+		}
 	}
 }
--- src/main/java/com/khubla/acme/listener/RoleTypeRefListener.java
+++ src/main/java/com/khubla/acme/listener/RoleTypeRefListener.java
@@ -1,10 +1,20 @@
 package com.khubla.acme.listener;
 
+import java.util.*;
+
 import com.khubla.acme.acmeParser.*;
 
 public class RoleTypeRefListener extends AbstractListener {
+	public List<String> names = new ArrayList<String>();
+
 	@Override
 	public void enterAcmeRoleTypeRef(AcmeRoleTypeRefContext ctx) {
-		throw new RuntimeException("Not Implemented");
+		if (null != ctx.identifier()) {
+			for (IdentifierContext identifierContext : ctx.identifier()) {
+				IdentifierListener identifierListener = new IdentifierListener();
+				identifierListener.enterIdentifier(identifierContext);
+				names.add(identifierListener.identifier);
+			}
+		}
 	}
 }

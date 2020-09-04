9d31beeaefc180fd3d5acef761823da2bca5f631
Tom Everett <tom@khubla.com>
2020-09-04 13:51:39 -0600

    all test cases parse

--- src/main/java/com/khubla/acme/listener/SetConstructorListener.java
+++ src/main/java/com/khubla/acme/listener/SetConstructorListener.java
@@ -1,10 +1,47 @@
 package com.khubla.acme.listener;
 
 import com.khubla.acme.*;
+import com.khubla.acme.acmeParser.*;
 
 public class SetConstructorListener extends AbstractListener {
 	@Override
 	public void enterSetConstructor(acmeParser.SetConstructorContext ctx) {
-		throw new RuntimeException("Not Implemented");
+		/*
+		 * var set
+		 */
+		if (null != ctx.variableSetDeclaration()) {
+			final VariableSetDeclarationListener variableSetDeclarationListener = new VariableSetDeclarationListener();
+			variableSetDeclarationListener.enterVariableSetDeclaration(ctx.variableSetDeclaration());
+		}
+		/*
+		 * DR
+		 */
+		if (null != ctx.designRuleExpression()) {
+			final DesignRuleExpressionListener designRuleExpressionListener = new DesignRuleExpressionListener();
+			designRuleExpressionListener.enterDesignRuleExpression(ctx.designRuleExpression());
+		}
+		/*
+		 * ref
+		 */
+		if (null != ctx.reference()) {
+			for (final ReferenceContext referenceContext : ctx.reference()) {
+				final ReferenceListener referenceListener = new ReferenceListener();
+				referenceListener.enterReference(referenceContext);
+			}
+		}
+		/*
+		 * type
+		 */
+		if (null != ctx.acmeTypeRef()) {
+			final TypeRefListener typeRefListener = new TypeRefListener();
+			typeRefListener.enterAcmeTypeRef(ctx.acmeTypeRef());
+		}
+		/*
+		 * set expr
+		 */
+		if (null != ctx.setExpression()) {
+			final SetExpressionListener setExpressionListener = new SetExpressionListener();
+			setExpressionListener.enterSetExpression(ctx.setExpression());
+		}
 	}
 }
